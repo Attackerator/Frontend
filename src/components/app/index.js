0dc4a23ed1b4972aaa3004ec09bfbc6e6c9a32e3
Dustin <dyelland@gmail.com>
2017-10-23 14:01:54 -0500

    set up app component for styling

---
+++ src/components/app/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Switch,Link } from 'react-router-dom';
+import createAppStore from '../../lib/store';
+import { Provider } from 'react-redux';
+
+const store = createAppStore();
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Provider store={store}>
+          <Router>
+            <section>
+              <nav>
+                <ul>
+                  <li><Link to={'/'}>Home</Link></li>
+                  <li><Link to={'/home/signin'}>Sign Up</Link></li>
+                </ul>
+              </nav>
+              <div>
+                <button>Random Button</button>
+                <button>Random Button</button>
+                <button>Random Button</button>
+                <input placeholder="InputField"/>
+                <input placeholder="InputField"/>
+                <input placeholder="InputField"/>
+              </div>
+            </section>
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
+}
