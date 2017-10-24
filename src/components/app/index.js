b8fb2d19fd3b0e996f6f5b1a1f128274acfde6b5
Dustin <dyelland@gmail.com>
2017-10-23 20:13:45 -0500

    moved store/provider to main.js

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Switch,Link } from 'react-router-dom';
-import createAppStore from '../../lib/store';
-import { Provider } from 'react-redux';
+import { BrowserRouter as Router,Route,Link } from 'react-router-dom';
 
 import SignInForm from '../signin-form';
-
-const store = createAppStore();
-
 import DashboardContainer from '../dashboard';
 
 export default class App extends React.Component {
   render(){
     return(
-      <div>
-        <Provider store={store}>
-          <Router>
-            <section>
-              <div>
-                <Route exact path='/dashboard' component={DashboardContainer} />
-                <Route exact path='/home/signin' component={SignInForm}/>
-              </div>
-              <nav>
-                <ul>
-                  <li><Link to={'/'}>Home</Link></li>
-                  <li><Link to={'/home/signin'}>Sign Up</Link></li>
-                  <li><Link to={'/dashboard'}>Dashboard</Link></li>
-                </ul>
-              </nav>
-              <div>
-                <button>Random Button</button>
-                <button>Random Button</button>
-                <button>Random Button</button>
-                <input placeholder="InputField"/>
-                <input placeholder="InputField"/>
-                <input placeholder="InputField"/>
-              </div>
-            </section>
-          </Router>
-        </Provider>
+      <div className="app">
+        <div>
+          <Route exact path='/dashboard' component={DashboardContainer} />
+          <Route exact path='/home/signin' component={SignInForm}/>
+        </div>
+        <nav>
+          <ul>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/home/signin'}>Sign Up</Link></li>
+            <li><Link to={'/dashboard'}>Dashboard</Link></li>
+          </ul>
+        </nav>
+        <div>
+          <button>Random Button</button>
+          <button>Random Button</button>
+          <button>Random Button</button>
+          <input placeholder="InputField"/>
+          <input placeholder="InputField"/>
+          <input placeholder="InputField"/>
+        </div>
       </div>
     );
   }
