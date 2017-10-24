43c8f617ec08978b84cf110f691f93d2a341238b
Dustin <dyelland@gmail.com>
2017-10-24 14:48:21 -0500

    added redux dev tools for browser

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,12 +1,11 @@
 import { createStore,applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
 import thunk from './redux-thunk';
 import reporter from './redux-reporter';
 
-
-
 export const appStoreCreate = () =>
-  createStore(reducers,
-    applyMiddleware(thunk,reporter));
+  createStore(reducers,composeWithDevTools(
+    applyMiddleware(thunk,reporter)));
 
 export default appStoreCreate;
