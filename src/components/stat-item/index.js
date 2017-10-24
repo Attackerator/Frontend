c3de9de179c295502d5717a1b7c04189474d1574
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-24 10:48:10 -0500

    add modifier to stat items

--- src/components/stat-item/index.js
+++ src/components/stat-item/index.js
@@ -12,7 +12,8 @@ export default class StatItem extends React.Component {
       <li className="statItem">
         <button className='roll'>Roll</button>
         <h3>{this.props.statName}</h3>
-        <span>{this.props.value}</span>
+        <span className="value">{this.props.value}</span>
+        <span className="modifier">{Math.floor((this.props.value-10)/2)}</span>
         <button className='edit'>Edit</button>
       </li>
     );
