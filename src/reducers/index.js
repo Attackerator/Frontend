c6df7a64ebe0b341f81b470eced2b94e2202cf1a
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-24 15:41:09 -0500

    save componenets

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,7 +10,7 @@ const defaultState = {
   currentCharacter: {
     name: 'Horatio Hornblower',
     attacks: [{name:'bash', stat: 'strength', damageType: 'squishing', diceType: 6, diceCount: 2, description: 'smoosh', toHitBonus: 4, damageBonus: 1, characterId: 41, userId: 'abc', _id: 124}],
-    saves: [],
+    saves: [{_id: 432901, type: 'fortitude', bonus: -2, stat: 'constitution', characterId: 41, userId: 'abc'}],
     skills: [ {_id: 3198, name: 'medicine', stat: 'intelligence', bonus: 14, characterId: 41, userId: 'abc' } ],
     spells: [{name:'I\'m a chargin ma Lazer', stat: 'charisma', damageType: 'zappy', diceType: 20, diceCount: 14, description: 'pew pew lazers shoot all over the place', toHitBonus: 0, damageBonus: 42, characterId: 41, userId: 'abc', _id: 121}],
     stats: [{strength: 50, dexterity: 10, constitution: 3, intelligence: 14, wisdom: 32, charisma: 9001, _id: 123, characterId: 41, userId: 'abc'}],
