ca44d64ee62fe7c9822cc49392a65628f0f1b48e
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-25 16:47:07 -0500

    check to make sure user is logged in before rendering dashboard

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,41 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Link,Redirect } from 'react-router-dom';
 
 import SignInForm from '../signin-form';
 import SignUpForm from '../signup-form';
 import DashboardContainer from '../dashboard';
 import { get_cookie } from '../../lib/helper';
 
-function loggedIn() {
-  let token = get_cookie('token');
-  if(token){
-    return true;
-  }
-  return false;
+function isloggedIn() {
+  console.log(get_cookie('token'));
+  return get_cookie('token');
 }
-
-function requireAuth(nextState, replace) {
-  if (!loggedIn()) {
-    replace({
-      pathname: '/login'
-    });
-  }
-}
-
 
 export default class App extends React.Component {
 
   render(){
     return(
       <div className="app">
-        <div>
-          <Route exact path='/' component={DashboardContainer} onEnter={requireAuth}/>
           <Route exact path='/login' component={SignInForm}/>
+          <Route exact path='/dashboard' render={() => (isloggedIn() ?  (<DashboardContainer />) : (<Redirect to='/login'/>))}/>
           <Route exact path='/home/signup' component={SignUpForm}/>
-        </div>
         <nav>
           <ul>
-            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/dashboard'}>Home</Link></li>
             <li><Link to={'/home/signup'}>Sign Up</Link></li>
             <li><Link to={'/login'}>Sign In</Link></li>
           </ul>
