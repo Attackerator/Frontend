77a972e48c536d05609963b6bffe72fa6d749c5e
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-25 16:14:50 -0500

    check authentication in route

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,13 +4,32 @@ import { BrowserRouter as Router,Route,Link } from 'react-router-dom';
 import SignInForm from '../signin-form';
 import SignUpForm from '../signup-form';
 import DashboardContainer from '../dashboard';
+import { get_cookie } from '../../lib/helper';
+
+function loggedIn() {
+  let token = get_cookie('toekn');
+  if(token){
+    return true;
+  }
+  return false;
+}
+
+function requireAuth(nextState, replace) {
+  if (!loggedIn()) {
+    replace({
+      pathname: '/login'
+    });
+  }
+}
+
 
 export default class App extends React.Component {
+
   render(){
     return(
       <div className="app">
         <div>
-          <Route exact path='/dashboard' component={DashboardContainer} />
+          <Route exact path='/' component={DashboardContainer} onEnter={requireAuth}/>
           <Route exact path='/login' component={SignInForm}/>
           <Route exact path='/home/signup' component={SignUpForm}/>
         </div>
@@ -19,7 +38,6 @@ export default class App extends React.Component {
             <li><Link to={'/'}>Home</Link></li>
             <li><Link to={'/home/signup'}>Sign Up</Link></li>
             <li><Link to={'/login'}>Sign In</Link></li>
-            <li><Link to={'/dashboard'}>Dashboard</Link></li>
           </ul>
         </nav>
       </div>
