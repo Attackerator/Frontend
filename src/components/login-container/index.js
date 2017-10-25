063a6d14b9ea636e19e1429b555471e54b5f9f80
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-25 17:01:10 -0500

    login-container component created

---
+++ src/components/login-container/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import SignInForm from '../signin-form';
+import SignUpForm from '../signup-form';
+
+export default class LoginContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      signIn: true
+    };
+
+    this.toggleSignIn = this.toggleSignIn.bind(this);
+  }
+
+  toggleSignIn(e) {
+    e.preventDefault();
+    this.setState({
+      signIn: !this.state.signIn
+    });
+  }
+
+  render(){
+    return(
+      <div className="signup-signin">
+        {
+          this.state.signIn  ?
+            (
+            <div>
+              <SignInForm/>
+              <span className="signUp"><a href="#" onClick={this.toggleSignIn}>Create an Account</a></span>
+            </div>
+            ) :
+            (
+            <div>
+              <SignUpForm/>
+              <span className="signUp"><a href="#" onClick={this.toggleSignIn}>Create an Account</a></span>
+            </div>
+            )
+        }
+      </div>
+    );
+  }
+}
