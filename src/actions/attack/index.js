e1ce26149330ddb33f5d7da6332a272794bb2132
Dustin <dyelland@gmail.com>
2017-10-26 18:54:53 -0500

    corrected action discrepencies

--- src/actions/attack/index.js
+++ src/actions/attack/index.js
@@ -3,33 +3,33 @@ import { get_cookie } from '../../lib/helper';
 
 const request = require('superagent');
 
-export const postAttackRequest = attack => dispatch => {
+export const postAttackRequest = (charId,attack) => dispatch => {
   let token = get_cookie('token');
   console.log(token);
-  return request.post(`${API_URL}/api/attack`)
-  .set({Authorization: `Bearer ${token}`})
-  .send(attack)
-  .then(res => {
-    dispatch(character.getCharacterRequest(res.body.characterId));
-  });
+  return request.post(`${API_URL}/api/attack/${charId}`)
+    .set({Authorization: `Bearer ${token}`})
+    .send(attack)
+    .then(res => {
+      dispatch(character.getCharacterRequest(res.body.characterId));
+    });
 };
 
 export const putAttackRequest = (oldAttack, newAttack) => dispatch => {
   let token = get_cookie('token');
   console.log({oldAttack, newAttack, token});
   return request.put(`${API_URL}/api/attack/${oldAttack._id}`)
-  .set({Authorization: `Bearer ${token}`})
-  .send(newAttack)
-  .then(res => {
-    dispatch(character.getCharacterRequest(oldAttack.characterId));
-  });
+    .set({Authorization: `Bearer ${token}`})
+    .send(newAttack)
+    .then(res => {
+      dispatch(character.getCharacterRequest(oldAttack.characterId));
+    });
 };
 
 export const deleteAttackRequest = attack => dispatch => {
   let token = get_cookie('token');
   return request.delete(`${API_URL}/api/attack/${attack._id}`)
-  .set({Authorization: `Bearer ${token}`})
-  .then(res => dispatch => {
-    dispatch(character.getCharacterRequest(attack.characterId));
-  });
+    .set({Authorization: `Bearer ${token}`})
+    .then(res => {
+      dispatch(character.getCharacterRequest(attack.characterId));
+    });
 };
