29049582d5e94ffa7575a81afcc509d0943ed491
Meepedy <david@psyon.org>
2017-10-26 18:34:17 -0500

    added handleChange method to saveitem for handling changes in forms

--- src/components/save-item/index.js
+++ src/components/save-item/index.js
@@ -28,6 +28,12 @@ export default class SaveItem extends React.Component {
     });
   }
 
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render(){
     return(
       <div className="saveItem">
