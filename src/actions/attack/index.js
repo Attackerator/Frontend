f74f720333b15a6645d987a3f50b76aedddc9aa5
Meepedy <david@psyon.org>
2017-10-26 12:12:23 -0500

    action for attack put request

--- src/actions/attack/index.js
+++ src/actions/attack/index.js
@@ -4,6 +4,24 @@ import { get_cookie } from '../../lib/helper';
 const request = require('superagent');
 
 export const postAttackRequest = attack => dispatch => {
-    let token = get_cookie('token');
+  let token = get_cookie('token');
+  console.log(token);
+  return request.post(`${API_URL}/api/attack`)
+  .set({Authorization: `Bearer ${token}`})
+  .send({ name: 'Wham Wham', stat: 'strength', damageType: 'blunt', diceType: 20,
+    diceCount: 20, toHitBonus: 20, damageBonus: 200 })
+  .then(res => {
+    dispatch(character.getCharacterRequest(res.body.characterId));
+  });
+};
 
-}
+export const putAttackRequest = (oldAttack, newAttack) => dispatch => {
+  let token = get_cookie('token');
+  console.log({oldAttack, newAttack, token});
+  return request.put(`${API_URL}/api/attack/${oldAttack._id}`)
+  .set({Authorization: `Bearer ${token}`})
+  .send(newAttack)
+  .then(res => {
+    dispatch(character.getCharacterRequest(oldAttack.characterId));
+  });
+};
