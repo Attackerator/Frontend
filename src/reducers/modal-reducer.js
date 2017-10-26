7980c9d93f95bc4b51dff799aa588e1ab54fd5af
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-10-26 14:39:08 -0500

    reducer for modal

---
+++ src/reducers/modal-reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+  modalType: null,
+  modalProps: {}
+};
+const defaultAction = {};
+
+export default(state = initialState, action = defaultAction) => {
+  switch (action.type) {
+    case 'SHOW_MODAL':
+      return {
+        modalType: action.modalType,
+        modalProps: action.modalProps
+      };
+    case 'HIDE_MODAL':
+      return initialState;
+    default:
+      return state;
+  }
+};
