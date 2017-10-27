72fec4de80abf8db7de39919d5dc81604f40ce25
Meepedy <david@psyon.org>
2017-10-26 22:13:54 -0500

    fixed broken routes from merge

--- src/actions/stat/index.js
+++ src/actions/stat/index.js
@@ -3,20 +3,21 @@ import { get_cookie } from '../../lib/helper';
 
 const request = require('superagent');
 
-<<<<<<< 3a8c08951c5d989fa0c5ae4ba1ed65cd4e58725b
 export const putStatsRequest = (id,newStats) => dispatch => {
   let token = get_cookie('token');
   return request.put(`${API_URL}/api/stats/${id}`)
     .set({Authorization: `Bearer ${token}`})
     .send(newStats)
-=======
+    .then(res => {
+      dispatch(character.getCharacterRequest(res.body.characterId));
+    });
+};
+
 export const postStatRequest = (charId,stat) => dispatch => {
   let token = get_cookie('token');
-  console.log(token);
   return request.post(`${API_URL}/api/stats/${charId}`)
     .set({Authorization: `Bearer ${token}`})
     .send(stat)
->>>>>>> added post route for stats
     .then(res => {
       dispatch(character.getCharacterRequest(res.body.characterId));
     });
