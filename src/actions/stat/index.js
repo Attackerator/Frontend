464481773fe80337787ea430a7b7678584851f9d
Meepedy <david@psyon.org>
2017-10-26 21:52:49 -0500

    added post route for stats

--- src/actions/stat/index.js
+++ src/actions/stat/index.js
@@ -3,11 +3,20 @@ import { get_cookie } from '../../lib/helper';
 
 const request = require('superagent');
 
+<<<<<<< 3a8c08951c5d989fa0c5ae4ba1ed65cd4e58725b
 export const putStatsRequest = (id,newStats) => dispatch => {
   let token = get_cookie('token');
   return request.put(`${API_URL}/api/stats/${id}`)
     .set({Authorization: `Bearer ${token}`})
     .send(newStats)
+=======
+export const postStatRequest = (charId,stat) => dispatch => {
+  let token = get_cookie('token');
+  console.log(token);
+  return request.post(`${API_URL}/api/stat/${charId}`)
+    .set({Authorization: `Bearer ${token}`})
+    .send(stat)
+>>>>>>> added post route for stats
     .then(res => {
       dispatch(character.getCharacterRequest(res.body.characterId));
     });
